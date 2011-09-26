c1dfcccf7b34adbbcfa312c1095af46e45cec1f6
Jim O'Regan <joregan@gmail.com>
2011-09-26 00:15:37 +0000

    make this more easily moveable

--- dev/chunk-to-post.sh
+++ dev/chunk-to-post.sh
@@ -1,3 +1,5 @@
 #!/bin/sh 
-    /usr/local/bin/apertium-transfer /usr/local/share/apertium/apertium-eu-en/apertium-eu-en.eu-en.t1x  /usr/local/share/apertium/apertium-eu-en/eu-en.t1x.bin  /usr/local/share/apertium/apertium-eu-en/eu-en.autobil.bin |/usr/local/bin/apertium-interchunk /usr/local/share/apertium/apertium-eu-en/apertium-eu-en.eu-en.t2x  /usr/local/share/apertium/apertium-eu-en/eu-en.t2x.bin |/usr/local/bin/apertium-postchunk /usr/local/share/apertium/apertium-eu-en/apertium-eu-en.eu-en.t3x  /usr/local/share/apertium/apertium-eu-en/eu-en.t3x.bin 
+BIN="/usr/local/bin"
+EUEN="/usr/local/share/apertium/apertium-eu-en"
+$BIN/apertium-transfer $EUEN/apertium-eu-en.eu-en.t1x  $EUEN/eu-en.t1x.bin  $EUEN/eu-en.autobil.bin |$BIN/apertium-interchunk $EUEN/apertium-eu-en.eu-en.t2x  $EUEN/eu-en.t2x.bin |$BIN/apertium-postchunk $EUEN/apertium-eu-en.eu-en.t3x  $EUEN/eu-en.t3x.bin 
   
